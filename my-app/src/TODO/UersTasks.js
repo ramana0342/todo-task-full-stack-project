68705f9c8abc896b43e80f5463e6609f931f1bff
Ramana Reddy <ramanareddy.m0342@gmail.com>
2024-06-05 11:29:10 +0530

    The commit for empty Div

--- my-app/src/TODO/UersTasks.js
+++ my-app/src/TODO/UersTasks.js
@@ -58,7 +58,7 @@ const UserTasks = ({ setUpdateFun,setUpdateTask, DOMUpdate, setDOMUpdate ,Update
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" /></svg></div>}
                                      
-                                        { UpdateStatus===item._id ? <button class="btn btn-primary" type="button" disabled><span class="spinner-grow spinner-grow-sm" aria-hidden="true"></span><span role="status">Updating...</span></button> 
+                                        { UpdateStatus===item._id ? <button style={{marginTop:"0px",paddingTop:"0px" ,height:"30px"}} class="btn btn-primary" type="button" disabled><span class="spinner-grow spinner-grow-sm" aria-hidden="true"></span><span role="status">Updating...</span></button> 
                                         : <div onClick={() => { UpdateSymbolClick(item._id, item.Task) }}><i style={{ fontSize: "25px", cursor:"pointer"}} class="fa-solid fa-pen-to-square"></i></div> }
                                 </div>
                             </div>
@@ -166,7 +166,7 @@ const UserActivity = () => {
                         { BtnStatus ?  <button class="btn btn-primary" type="button" disabled><span class="spinner-border spinner-border-sm" aria-hidden="true"></span><span role="status">Loading...</span></button>
                         : <input onClick={() => { AddTaskEvent() }} class="btn btn-primary" type="submit" value="Add Task" id="submitBtn" /> }</>:
 
-                            <><input onChange={(e) => { updateHandleChange("Task", e.target.value) }} type="text" class="form-control" value={updateTask.Task} placeholder="Type Any Task"/>
+                            <><input onChange={(e) => { updateHandleChange("Task", e.target.value) }} type="text" class="form-control" value={updateTask.Task} placeholder="Update Your Task"/>
                                 {BtnStatus ? <button class="btn btn-warning" type="button" disabled><span class="spinner-grow spinner-grow-sm" aria-hidden="true"></span><span role="status">Loading...</span></button>
                                 :<input onClick={() => { UpdatingTask(UpdateFun.TaskId) }} class="btn btn-warning" type="submit" value="Update" id="submitBtn" />}</>}
                     </div>
